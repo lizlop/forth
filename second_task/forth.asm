a6e1b7bcf2243be62d42ebfd46de8561f8c54701
Elizaveta <37666431+lizlop@users.noreply.github.com>
2019-06-13 11:56:37 +0300

    add and update files

--- second_task/forth.asm
+++ second_task/forth.asm
@@ -1,24 +1,27 @@
-%include "macro.inc"
-%include "basic_words.inc"
-%include "initial_process.inc"
-
 global _start
+%include "macro.inc"
 %define pc r15
 %define w r14
 %define rstack r13
+
+section .text
+%include "basic_words.inc"
+%include "initial_process.inc"
 
 section .bss
 resq 1023
 rstack_start: resq 1
 stack_start: resq 1
 in_fd: resq 1
-input_buffer: resb 1024		; buffer to read textual words into
+input_buf: resb 1024		; buffer to read textual words into
 user_dictionary:  resq 65536	; data for words
 user_mem: resq 65536		; global data for user
 state: resq 1			; changes to 1 if compiling, 0 by default
 
 section .data
 dict_pointer: dq user_dictionary
+mem_pointer: dq user_mem
+last_word: dq _lw
 
 section .rodata
 error_message: db ": no such word", 10, 0
@@ -32,4 +35,7 @@ next:	mov w, [pc]
 	add pc, 8
 	jmp [w]
 
-program: dq xt_start
+program:
+	dq xt_lit, 0
+	dq xt_interpret
+	dq xt_bye
